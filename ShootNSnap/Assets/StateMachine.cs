6d26070ddb1d6cccc80cb88b6382bd15b0c2d385
Jeremy <jeremy.ries@fluffyfairygames.com>
2019-05-26 18:07:42 +0200

    adding graphics

--- ShootNSnap/Assets/StateMachine.cs
+++ ShootNSnap/Assets/StateMachine.cs
@@ -3,103 +3,63 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+ 
+public class Level : MonoBehaviour
+{
+	public Transform PlayerSpawn;
+}
+
 public class StateMachine : MonoBehaviour
 {
 	private enum PlayerStates
 	{
 		Spawning,
-		Moving,
-		Falling,
-		OnGround,
-		StandingUp,
+		AllowShootInput,
+		Flying,
+		Snapped,
+		Death
 	}
 
-	[SerializeField] private Transform _body;
-	[SerializeField] private Transform _bottomAnchor;
+	[SerializeField] private Level _level;
 
-	[SerializeField] private float _acceleration;
-	[SerializeField] private float _criticalFallingSpeed;
+	[SerializeField] private Transform _body;
 
 	[SerializeField] private Text _debugStateText;
 	
 	private float _speed;
-	
 
-	[SerializeField] private Dictionary<PlayerStates,float> _durationForState;
-	private Dictionary<PlayerStates,float> _currentTimeInState = new Dictionary<PlayerStates, float>();
-	
-	private PlayerStates _state = PlayerStates.Moving;
-	private PlayerStates State
-	{
-		get { return _state;}
-		set
-		{
-			_currentTimeInState[_state] = 0;
-			_state = value;
-		}
-	}
-	
+
+	private PlayerStates State { get; set; } = PlayerStates.Spawning;
+
 	void Update ()
 	{
-		_debugStateText.text = State.ToString();
-		_currentTimeInState[_state] += Time.deltaTime;
-			
+
 		switch (State)
 		{
 			case PlayerStates.Spawning:
 				HandleSpawning();
 				break;
-			case PlayerStates.Moving:
-				HandleMoving();
-				break;
-			case PlayerStates.Falling:
-				HandleFalling();
-				break;
-			case PlayerStates.OnGround:
-				HandleOnGround();
-				break;
-			case PlayerStates.StandingUp:
-				break;
+
 			default:
 				throw new ArgumentOutOfRangeException();
-		}
-		
-	}
-
-	private void HandleOnGround()
-	{
-		if (_currentTimeInState[PlayerStates.OnGround] > _durationForState[PlayerStates.OnGround])
-		{
-			State = PlayerStates.Moving;
-		}
+		}	
 	}
 
 	private void HandleSpawning()
 	{
-		//todo play falling animation
-		State = PlayerStates.Moving;
+		transform.position = _level.PlayerSpawn.position;
 	}
 
-	private void HandleFalling()
-	{
-		if (_currentTimeInState[PlayerStates.Falling] > _durationForState[PlayerStates.Falling])
-		{
-			State = PlayerStates.OnGround;
-		}
-	}
+//	private void HandleFalling()
+//	{
+//		if (_currentTimeInState[PlayerStates.Falling] > _durationForState[PlayerStates.Falling])
+//		{
+//			State = PlayerStates.OnGround;
+//		}
+//	}
 
 	private void HandleMoving()
 	{
-		var xinput = Input.GetAxis("Horizontal");
-        		
-		_bottomAnchor.localRotation = Quaternion.AngleAxis(-xinput *50, new Vector3(0,0,1));
 
-		_speed += xinput * _acceleration;
-		_body.transform.position += Vector3.right * _speed;
-
-		if (_speed > _criticalFallingSpeed)
-		{
-			State = PlayerStates.Falling;
-		}
 	}
 }
